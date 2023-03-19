054953ae90ae70b34802a56ca09e6d7d42608e4f
Vincent Palmer <shift@someone.section.me>
2023-03-19 17:54:42 +0100

    fix(editor): Please stop 'auto correcting' me perfectly vaild code, code servers.

--- base.sh
+++ base.sh
@@ -6,7 +6,7 @@ RELEASE=$(echo "$VERSION" | cut -d '.' -f 1-2)
 MANIFEST=ghcr.io/oci-base/alpine
 
 buildah manifest create "$MANIFEST:v$VERSION"
-for arch in "$(echo "$ARCHITECTURES")"; do
+for arch in $(echo $ARCHITECTURES); do
   ctr=$(buildah from --arch "$arch" scratch)
   buildah add "$ctr" https://dl-cdn.alpinelinux.org/alpine/"$RELEASE/releases/$arch/alpine-netboot-$VERSION-$arch".tar.gz
   buildah commit --manifest "$MANIFEST:v$VERSION" "$ctr" "$MANIFEST:$VERSION-$arch"
